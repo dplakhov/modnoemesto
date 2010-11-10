822c6c1be4603fe7d02ca143751bed1e258b852e
evil <evil@web-mark.ru>
2010-11-10 04:00:22 +0300

    Add Account.fs_offers_inbox_count field and keep it actual.

--- apps/social/documents.py
+++ apps/social/documents.py
@@ -26,6 +26,7 @@ class Account(User):
 
     # some denormalisation
     friends_count = IntField(default=0)
+    fs_offers_inbox_count = IntField(default=0)
     unread_msg_count = IntField(default=0)
     msg_inbox_count = IntField(default=0)
     msg_sent_count = IntField(default=0)
@@ -47,16 +48,22 @@ class Account(User):
                 if acc.friends_count > 499:
                     raise LimitsViolationException(acc)
 
-            FriendshipOffer.objects(recipient=self, author=user).delete()
+            FriendshipOffer.objects.get(recipient=self,
+                                        author=user).delete(is_accepted=True)
 
             Account.objects(id=user.id).update_one\
                     (add_to_set__mutual_friends=self, inc__friends_count=1,
                      inc__version=1)
             Account.objects(id=self.id).update_one\
                     (add_to_set__mutual_friends=user, inc__friends_count=1,
-                     inc__version=1)
+                     dec__fs_offers_inbox_count=1, inc__version=1)
         else:
-            FriendshipOffer.objects.get_or_create(recipient=user, author=self)
+            #@warning: this code is non-transactional too
+            _, created = FriendshipOffer.objects.get_or_create(recipient=user,
+                                                    author=self)
+            if created:
+                Account.objects(id=user.id).update_one\
+                    (inc__fs_offers_inbox_count=1, inc__version=1)
 
 
 
@@ -147,6 +154,16 @@ class FriendshipOffer(Document):
     def __init__(self, *args, **kwargs):
         super(FriendshipOffer, self).__init__(*args, **kwargs)
         self.timestamp = self.timestamp or datetime.now()
+
+    def delete(self, is_accepted=False):
+        """
+        @param is_accepted: if False (default), also decreases recipient's
+        fs_offers_inbox_count field value
+        """
+        if not is_accepted:
+            Account.objects(id=self.recipient.id).update_one\
+                    (dec__fs_offers_inbox_count=1, inc__version=1)
+        super(FriendshipOffer, self).delete()
 
 
 class Message(Document):
