0aeffa5e6c8421a938fc357c1be18811521c1c40
dgk <dgk@web-mark.ru>
2010-11-17 13:40:44 +0300

    Fabric.fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+from fabric.api import run, local, cd
+        
+def deploy():
+    local('git push')
+    with cd('/var/www/'):
+        run('git pull')
+        run('/etc/init.d/apache2 reload')
+
+def host_type():
+    run('uname -s')
+
