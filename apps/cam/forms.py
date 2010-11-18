858207fbd9e5328a599011e6db0309315ee8e53f
dgk <dgk@web-mark.ru>
2010-11-18 13:44:25 +0300

    Cam driver checking before save

--- apps/cam/forms.py
+++ apps/cam/forms.py
@@ -1,12 +1,27 @@
 # -*- coding: utf-8 -*-
 
 from django import forms
-#from wtforms.ext.mongoengine.orm import model_form
-from .models import CameraType
+from django.utils.translation import ugettext_lazy as _
+
+from .models import Camera, CameraType
+from .drivers.base import Driver as BaseDriver
+from .drivers.exceptions import ImproperlyConfigured, AccessDenied
+
 
 class CameraTypeForm(forms.Form):
     name = forms.CharField()
     driver = forms.CharField()
+
+    def clean_driver(self):
+        driver = self.cleaned_data['driver']
+        camera_type = CameraType(driver=driver)
+        try:
+            driver_class = camera_type.driver_class
+            assert issubclass(driver_class, BaseDriver)
+        except:
+            raise forms.ValidationError(_('Invalid driver name %(driver)s' % dict(driver=driver)))
+        return driver
+
 
 class CameraForm(forms.Form):
     name = forms.CharField()
@@ -16,5 +31,16 @@ class CameraForm(forms.Form):
     password = forms.CharField()
     enabled = forms.BooleanField(required=False)
 
+    def clean(self):
+        data = self.cleaned_data
+        args = dict([(x, data[x]) for x in 'name host username password'.split()])
+        args['type'] = CameraType.objects.get(id=data['type'])
+        cam = Camera(**args)
+        try:
+            cam.driver.control.check()
+        except AccessDenied:
+            raise forms.ValidationError(_('Invalid camera credentials'))
+        except ImproperlyConfigured:
+            raise forms.ValidationError(_('Camera improperly configured'))
+        return self.cleaned_data
 
-#CameraTypeForm = model_form(CameraType)
