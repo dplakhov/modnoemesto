db3289c88da1a2596501b48c6df0f84efcc78513
evil <evil@web-mark.ru>
2010-11-18 03:12:41 +0300

    Fix cam urls.

--- apps/cam/urls.py
+++ apps/cam/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('apps.cam.views',
     url(r'^$', 'cam_list', name='cam_list'),
-    url(r'^add/$', 'cam_edit', name='cam_edit'),
+    url(r'^add/$', 'cam_edit', name='cam_add'),
 
     url(r'^(?P<id>[a-f0-9]{24})/$', 'cam_view', name='cam_view'),
 
@@ -13,7 +13,7 @@ urlpatterns = patterns('apps.cam.views',
 
 
     url(r'^types/$', 'type_list', name='type_list'),
-    url(r'^types/add/$', 'type_edit', name='type_edit'),
+    url(r'^types/add/$', 'type_edit', name='type_add'),
     url(r'^types/(?P<id>[a-f0-9]{24})/$', 'type_edit', name='type_edit'),
     url(r'^types/(?P<id>[a-f0-9]{24})/delete/$', 'type_delete', name='type_delete'),
 
