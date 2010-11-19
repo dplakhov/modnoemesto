2034b090ef8d8820ec43f43cf134a83eb5f920cf
Ilya <elias@web-mark.ru>
2010-11-19 19:37:49 +0300

    Fix menu tag

--- apps/social/templatetags/menu_tags.py
+++ apps/social/templatetags/menu_tags.py
@@ -6,6 +6,7 @@ register = template.Library()
 
 @register.simple_tag
 def active(request, pattern):
-    if request.path.startswith(reverse(pattern)):
+    url = reverse(pattern)
+    if (url == "/" and request.path == url) or (url != "/" and request.path.startswith(url)):
         return 'menu_curent'
     return ''
