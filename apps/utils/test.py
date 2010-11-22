ebe8ccdb1d7384f8534f7ea12af3d5939e6ade1b
dgk <dgk@web-mark.ru>
2010-11-22 17:22:41 +0300

    apps.utils.test.patch_settins function

---
+++ apps/utils/test.py
@@ -0,0 +1,23 @@
+
+from contextlib import contextmanager
+
+class SettingDoesNotExist:
+    pass
+
+@contextmanager
+def patch_settings(**kwargs):
+    from django.conf import settings
+    old_settings = []
+    for key, new_value in kwargs.items():
+        old_value = getattr(settings, key, SettingDoesNotExist)
+        old_settings.append((key, old_value))
+        setattr(settings, key, new_value)
+    yield
+    for key, old_value in old_settings:
+        if old_value is SettingDoesNotExist:
+            delattr(settings, key)
+        else:
+            setattr(settings, key, old_value)
+
+
+
