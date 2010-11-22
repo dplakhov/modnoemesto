0c079497d81bbb3895b951d30c7075988668b0d4
dgk <dgk@web-mark.ru>
2010-11-22 15:50:03 +0300

    Separate mongo connections for test and prod

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,11 +1,13 @@
+import sys
+import mongoengine
+
 from dist import *
 try:
     from local import *
 except ImportError:
     pass
 
-import mongoengine
-mongoengine.connect('social')
+mongoengine.connect('social_test' if 'test' in sys.argv else 'social')
 
 import djcelery
 djcelery.setup_loader()
