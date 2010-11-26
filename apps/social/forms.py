ea9a1ebdabc99a9d752d251f628efc1c7b9da51b
Ilya <elias@web-mark.ru>
2010-11-26 15:42:20 +0300

    Fix auth, set django login

--- apps/social/forms.py
+++ apps/social/forms.py
@@ -3,10 +3,42 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 from documents import User
+from django.contrib.auth import authenticate
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=128, required=True)
+    username = forms.CharField(label=_("Username"), max_length=30)
     password = forms.CharField(label=_("Password"), widget=forms.PasswordInput)
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+        super(LoginForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(username=username, password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(_("Please enter a correct username and password. Note that both fields are case-sensitive."))
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(_("This account is inactive."))
+
+        # TODO: determine whether this should move to its own method.
+        if self.request:
+            if not self.request.session.test_cookie_worked():
+                raise forms.ValidationError(_("Your Web browser doesn't appear to have cookies enabled. Cookies are required for logging in."))
+
+        return self.cleaned_data
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
 
 
 class UserCreationForm(forms.Form):
