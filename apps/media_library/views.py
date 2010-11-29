739c55f6307a8a63c15fcd60a83e20817200b21f
dgk <dgk@web-mark.ru>
2010-11-29 12:11:44 +0300

    apps.utils.image.read_image_file() function

--- apps/media_library/views.py
+++ apps/media_library/views.py
@@ -6,8 +6,6 @@ try:
 except ImportError:
     from StringIO import StringIO
 
-from ImageFile import Parser as ImageFileParser
-
 from django.http import HttpResponse
 from django.views.generic.simple import direct_to_template
 from django.utils.translation import ugettext_lazy as _
@@ -15,12 +13,19 @@ from django.contrib import messages
 
 from apps.media.documents import File, FileSet
 
+from apps.utils.image import read_image_file
+
 from .forms import ImageAddForm
 
 
 LIBRARY_TYPE_IMAGE = 'image'
 LIBRARY_TYPE_AUDIO = 'audio'
 LIBRARY_TYPE_VIDEO = 'video'
+
+FILE_TYPE_IMAGE = 'image'
+FILE_TYPE_AUDIO = 'audio'
+FILE_TYPE_VIDEO = 'video'
+
 
 def get_library(type):
     assert type in (LIBRARY_TYPE_IMAGE, LIBRARY_TYPE_AUDIO, LIBRARY_TYPE_VIDEO, )
@@ -51,18 +56,17 @@ def image_add(request):
 
         buffer.reset()
         try:
-            parser = ImageFileParser()
-            parser.feed(buffer.read())
-            parser.close()
+            read_image_file(buffer)
         except Exception, e:
             messages.add_message(request, messages.ERROR, _('Invalid image file format'))
         else:
-            image = File(type='image', author=request.user)
+            image = File(type=FILE_TYPE_IMAGE, author=request.user)
             buffer.reset()
             image.file.put(buffer, content_type=file.content_type)
             image.save()
             library = get_library(LIBRARY_TYPE_IMAGE)
             library.add_file(image)
+
             messages.add_message(request, messages.SUCCESS, _('Image successfully added'))
 
     return redirect('media_library:image_index')
