39728412a8039805a6b2c1714aa467c11a85fc12
dgk <dgk@web-mark.ru>
2010-11-29 12:16:49 +0300

    trailing slash from media file view url removed

--- apps/media/urls.py
+++ apps/media/urls.py
@@ -2,5 +2,5 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('apps.media.views',
-   url(r'^(?P<file_id>[a-f0-9]{24})/(?P<transformation_name>[\w\.]+)/$', 'file_view', name='file_view'),
+   url(r'^(?P<file_id>[a-f0-9]{24})/(?P<transformation_name>[\w\.]+)$', 'file_view', name='file_view'),
 )
