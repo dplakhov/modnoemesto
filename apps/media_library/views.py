2707a6731c3d579d4ea69a8bf01fc7d8dd10a8c3
dgk <dgk@web-mark.ru>
2010-11-29 16:48:10 +0300

    Image library: file adding, index page

--- apps/media_library/views.py
+++ apps/media_library/views.py
@@ -1,31 +1,23 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import redirect
 
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from StringIO import StringIO
-
 from django.http import HttpResponse
 from django.views.generic.simple import direct_to_template
 from django.utils.translation import ugettext_lazy as _
 from django.contrib import messages
+from django.conf import settings
 
 from apps.media.documents import File, FileSet
 
 from apps.utils.image import read_image_file
+from apps.utils.stringio import StringIO
+
+from apps.media.transformations import BatchFileTransformation, ImageResize
+from apps.media.tasks import apply_file_transformations
 
 from .forms import ImageAddForm
 
-
-LIBRARY_TYPE_IMAGE = 'image'
-LIBRARY_TYPE_AUDIO = 'audio'
-LIBRARY_TYPE_VIDEO = 'video'
-
-FILE_TYPE_IMAGE = 'image'
-FILE_TYPE_AUDIO = 'audio'
-FILE_TYPE_VIDEO = 'video'
-
+from .constants import *
 
 def get_library(type):
     assert type in (LIBRARY_TYPE_IMAGE, LIBRARY_TYPE_AUDIO, LIBRARY_TYPE_VIDEO, )
@@ -66,6 +58,18 @@ def image_add(request):
             image.save()
             library = get_library(LIBRARY_TYPE_IMAGE)
             library.add_file(image)
+
+            transformations = [ ImageResize(name=name, format='png', width=w, height=h)
+                                    for (name, w, h) in (
+                ('library_image_thumbnail.png', 200, 100),
+                ('library_image_full.png', 400, 200),
+            )]
+
+            if settings.TASKS_ENABLED.get(LIBRARY_IMAGE_RESIZE_TASK):
+                args = [ image.id, ] + transformations
+                apply_file_transformations.apply_async(args=args)
+            else:
+                image.apply_transformations(*transformations)
 
             messages.add_message(request, messages.SUCCESS, _('Image successfully added'))
 
