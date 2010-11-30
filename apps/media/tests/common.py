f19c4d1fdb5aa8229e6f272e78b34cd41d479655
dgk <dgk@web-mark.ru>
2010-11-30 12:57:53 +0300

    apps.media.tests split

--- apps/media/tests/common.py
+++ apps/media/tests/common.py
@@ -2,6 +2,8 @@
 import os
 from ..documents import *
 
+from ..transformations import FileTransformation
+
 def file_path(file_name):
     return os.path.abspath(os.path.join(os.path.dirname(__file__),
                                         'files', file_name))
@@ -12,3 +14,12 @@ def create_file():
         content_type='image/png')
     file.save()
     return file
+
+class TextTransformation(FileTransformation):
+    def _apply(self, source, destination):
+        source_data = int(source.file.read())
+
+        destination.file.seek(0)
+        destination.file.replace(str(source_data * 2),
+                         content_type=destination.file.content_type)
+        destination.save()
--- apps/media/tests/tests.py
+++ apps/media/tests/tests.py
@@ -6,22 +6,11 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 import mongoengine
 
-from apps.utils.stringio import StringIO
-
 from ..documents import *
-from ..transformations import FileTransformation, BatchFileTransformation
 from ..transformations.image import ImageResize
 
 from .common import file_path, create_file
 
-class TextTransformation(FileTransformation):
-    def _apply(self, source, destination):
-        source_data = int(source.file.read())
-
-        destination.file.seek(0)
-        destination.file.replace(str(source_data * 2),
-                         content_type=destination.file.content_type)
-        destination.save()
 
 class FileTest(TestCase):
     def test_save_empty_file_raises_exc(self):
@@ -66,143 +55,6 @@ class FileViewTest(TestCase):
 
 
         self.assertEquals('image/png', response['Content-Type'])
-
-class FileTransformationTest(TestCase):
-    def test_apply_transformations(self):
-
-        TRANSFORMATION_NAME = 'thumbnail'
-
-        file = create_file()
-
-        file = File(id=file.id)
-        file.reload()
-
-        derivatives = file.apply_transformations(ImageResize(name=TRANSFORMATION_NAME,
-                                             format='png', width=100, height=100))
-
-        self.failUnless(derivatives)
-        self.failUnlessEqual(type({}), type(derivatives))
-        self.failUnlessEqual(1, len(derivatives))
-
-        derivative = derivatives[TRANSFORMATION_NAME]
-        derivative = File.objects(id=derivative.id).first()
-
-        self.failUnless(isinstance(derivative, File))
-        self.failUnless(derivative.file.read())
-
-        self.failUnlessEqual(derivative.file.read(),
-                             file.get_derivative(TRANSFORMATION_NAME).file.read())
-
-        self.failUnlessEqual(derivative.file.content_type, 'image/png')
-
-        derivative = file.get_derivative(TRANSFORMATION_NAME)
-
-        self.failUnlessEqual(TRANSFORMATION_NAME, derivative.transformation)
-
-        self.failUnlessEqual(file, derivative.source)
-
-        self.failUnlessRaises(File.DerivativeNotFound, file.get_derivative, 'notfound')
-
-        derivative = file.modifications[TRANSFORMATION_NAME]
-        self.failUnlessEqual(file, derivative.source)
-
-
-    def test_apply_transformations_before_save_raises_exc(self):
-        file = File(type='image')
-
-        file.file.put(open(file_path('logo-mongodb.png')),
-            content_type='image/png')
-
-        self.failUnlessRaises(Exception, file.apply_transformations,
-                              ImageResize(name='thumbnail',
-                                          format='png', width=100, height=100))
-
-    def test_apply_transformation_into_source(self):
-        file = File(type='text')
-        buffer = StringIO()
-        buffer.write('1')
-        buffer.reset()
-        file.file.put(buffer, content_type='text/plain')
-        file.save()
-        file.reload()
-        self.failUnlessEqual('1', file.file.read())
-
-
-        transformation = TextTransformation('test')
-        transformation.apply(file, file)
-
-        file.reload()
-        self.failUnlessEqual('2', file.file.read())
-
-        transformation.apply(file, file)
-        file.reload()
-        self.failUnlessEqual('4', file.file.read())
-
-class BatchFileTransformationTest(TestCase):
-    def test_batch_transformation_with_single_transformation(self):
-        file = File(type='text')
-        buffer = StringIO()
-        buffer.write('1')
-        buffer.reset()
-        file.file.put(buffer, content_type='text/plain')
-        file.save()
-        file.reload()
-
-        transformation = BatchFileTransformation('batch',
-                    TextTransformation('test')
-                )
-
-        transformation.apply(file, file)
-
-        file.reload()
-        self.failUnlessEqual('2', file.file.read())
-
-    def test_batch_transformation_with_multiple_transformations(self):
-        file = File(type='text')
-        buffer = StringIO()
-        buffer.write('1')
-        buffer.reset()
-        file.file.put(buffer, content_type='text/plain')
-        file.save()
-        file.reload()
-
-        transformation = BatchFileTransformation('batch',
-                    TextTransformation('test'),
-                    TextTransformation('test'),
-                    TextTransformation('test')
-                )
-
-        transformation.apply(file, file)
-
-        file.reload()
-        self.failUnlessEqual('8', file.file.read())
-
-    def test_batch_transformation_constructor_exc(self):
-        self.failUnlessRaises(Exception, BatchFileTransformation, 'batch')
-        self.failUnlessRaises(Exception, BatchFileTransformation, 'batch',
-                              'not Transformation class')
-
-
-class TransformationTasksTest(TestCase):
-    def test_apply_file_transformations(self):
-        print '\nthis test requres running celeryd (python manage.py celeryd test)'
-        from ..tasks import apply_file_transformations
-
-        file = create_file()
-
-        args = [
-            str(file.id),
-            ImageResize(name='trans1', format='png', width=100, height=100),
-            ImageResize(name='trans2', format='png', width=100, height=100)
-        ]
-
-        result = apply_file_transformations.apply_async(args=args)
-        result.get()
-
-        file.reload()
-
-        self.failUnless(file.get_derivative('trans1'))
-        self.failUnless(file.get_derivative('trans2'))
 
 
 class FileSetTest(TestCase):
