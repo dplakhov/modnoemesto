39afd3c67ba4c1f7f3092eeb06527ce20cb9e8c0
Ilya <elias@web-mark.ru>
2010-11-30 15:10:52 +0300

    Переместить настройки биллнга в отдельный модуль

--- settings/dist.py
+++ settings/dist.py
@@ -195,8 +195,7 @@ AVATAR_SIZES = (
 
 MAX_USER_MESSAGES_COUNT = 500
 
-PKSPB_DUSER = 'trend_test'
-PKSPB_DPASS = 'trend_test'
-PKSPB_ID = '291'
-
 LIBRARY_IMAGES_PER_PAGE = 2
+
+# settings include billing
+from billing import *
