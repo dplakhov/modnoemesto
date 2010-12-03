bd24544147af9328c7422f73a137990fffdd0aec
dgk <dgk@web-mark.ru>
2010-12-03 14:39:23 +0300

    Приложение friendship #10: User.friends.offers

--- apps/friends/documents.py
+++ apps/friends/documents.py
@@ -3,9 +3,7 @@
 from datetime import datetime
 
 from mongoengine.document import Document
-from mongoengine.fields import ReferenceField, StringField, BooleanField, DateTimeField, ListField
-
-from apps.social.documents import User
+from mongoengine.fields import ReferenceField, StringField, DateTimeField, ListField
 
 class FriendshipOffer(Document):
     author = ReferenceField('User')
@@ -17,6 +15,18 @@ class FriendshipOffer(Document):
         'indexes': ['author', 'recipient']
     }
 
+class FriendshipOfferList(object):
+    def __init__(self, user):
+        self.user = user
+
+    def send(self, user, message=''):
+        offer, created = FriendshipOffer.objects.get_or_create(author=self.user,
+                                       recipient=user,
+                                       defaults=dict(message=message)
+                                       )
+
+
+
 class Friendship(Document):
     friends = ListField(ReferenceField('User'))
     ctime = DateTimeField(default=datetime.now)
@@ -26,15 +36,20 @@ class UserFriends(Document):
     user = ReferenceField('User', unique=True)
     friends = ListField(ReferenceField('User'))
 
+    _offers = None
+
     @property
     def count(self):
         return 0
 
     def can_add(self, user):
-        return True
+        has_offers = FriendshipOffer.objects(author=self.user, recipient=user).count()
+        return not has_offers
 
-    def offer_send(self, user, message=''):
-        FriendshipOffer.objects.get_or_create(author=self.user,
-                                       recipient=user,
-                                       message=message
-                                       )
+    @property
+    def offers(self):
+        if self._offers is None:
+            self._offers = FriendshipOfferList(self.user)
+        return self._offers
+
+
--- apps/friends/tests.py
+++ apps/friends/tests.py
@@ -7,7 +7,7 @@ from django.test.client import Client
 from django.core.urlresolvers import reverse
 
 from apps.social.documents import User
-from .documents import FriendshipOffer, UserFriends
+from .documents import FriendshipOffer, UserFriends, FriendshipOfferList
 
 
 
@@ -34,131 +34,34 @@ class FriendshipTestCase(BasicTestCase):
     def test_friends_empty(self):
         user1 = self.user1
         user2 = self.user2
-        friends = user1.friends
-        self.failUnless(isinstance(friends, UserFriends))
-        self.failUnlessEqual(0, friends.count)
-        self.failUnless(friends.can_add(user2) is True)
+        self.failUnless(isinstance(user1.friends, UserFriends))
+        self.failUnlessEqual(0, user1.friends.count)
+        self.failUnless(user1.friends.can_add(user2) is True)
+        self.failUnless(isinstance(user1.friends.offers, FriendshipOfferList))
 
     def test_send_offer(self):
         user1 = self.user1
         user2 = self.user2
         friends = user1.friends
-        OFFER_MESSAGE = u'добавь меня'
-        friends.offer_send(user2, OFFER_MESSAGE)
+        OFFER_MESSAGE1 = u'добавь меня'
+        friends.offers.send(user2, OFFER_MESSAGE1)
         offer = FriendshipOffer.objects.get(author=user1, recipient=user2)
-        self.failUnlessEqual(OFFER_MESSAGE, offer.message)
+        self.failUnlessEqual(OFFER_MESSAGE1, offer.message)
 
+        self.failUnless(friends.can_add(user2) is False)
 
         OFFER_MESSAGE2 = u'почему не добавляешь-то??'
-        friends.offer_send(user2, OFFER_MESSAGE2)
+        friends.offers.send(user2, OFFER_MESSAGE2)
         self.failUnlessEqual(1,
                  FriendshipOffer.objects(author=user1, recipient=user2).count())
 
+        offer = FriendshipOffer.objects.get(author=user1, recipient=user2)
+        self.failUnlessEqual(OFFER_MESSAGE1, offer.message)
 
 
+    def test_accept_offer(self):
+        user1 = self.user1
+        user2 = self.user2
+        user1.friends.offers.send(user2)
 
 
-'''
-class FriendshipTestCase(BasicTestCase):
-
-    def setUp(self):
-        FriendshipOffer.objects.delete()
-        super(FriendshipTestCase, self).setUp()
-        self.resp = self.c.get(reverse('social:friend',
-                                       kwargs={ 'user_id': self.acc2.id }))
-
-    def tearDown(self):
-        FriendshipOffer.objects.delete()
-        super(FriendshipTestCase, self).tearDown()
-
-    def test_friendship_offer_creates(self):
-        self.assertEquals(self.resp.status_code, 302) # redirect
-        self.assertEquals(
-            FriendshipOffer.objects(author=self.acc1, recipient=self.acc2).count(), 1)
-
-    def test_friendship_offer_creates_once(self):
-        self.c.get(reverse('social:friend', kwargs={ 'user_id': self.acc2.id }))
-        self.assertEquals(
-            FriendshipOffer.objects(author=self.acc1, recipient=self.acc2).count(), 1)
-
-    def test_friendship_offer_increases_fs_offers_inbox_count(self):
-        self.c.get(reverse('social:friend', kwargs={ 'user_id': self.acc2.id }))
-        self.acc2.reload()
-        self.assertEquals(self.acc2.fs_offers_inbox_count, 1)
-
-    def test_friendship_offer_accepts_successfully(self):
-        self.c.login(username=self.acc2.username, password='123')
-        self.c.get(reverse('social:friend', kwargs={ 'user_id': self.acc1.id }))
-        self.acc1.reload(), self.acc2.reload()
-        self.assertTrue(self.acc2 in self.acc1.mutual_friends)
-        self.assertTrue(self.acc1 in self.acc2.mutual_friends)
-        self.assertEquals(FriendshipOffer.objects.count(), 0)
-
-    def test_accepting_friendship_offer_decreases_fs_offers_inbox_count(self):
-        self.c.login(username=self.acc2.username, password='123')
-        self.c.get(reverse('social:friend', kwargs={ 'user_id': self.acc1.id }))
-        self.acc2.reload()
-        self.assertEquals(self.acc2.fs_offers_inbox_count, 0)
-
-    def test_friendship_offer_cancels_successfully(self):
-        fso = FriendshipOffer.objects.get()
-        self.c.get(reverse('social:cancel_fs_offer',
-                           kwargs={ 'offer_id': fso.id }))
-        self.assertEquals(FriendshipOffer.objects.count(), 0)
-
-    def test_friendship_offer_cancelling_decreases_fs_offers_inbox_count(self):
-        fso = FriendshipOffer.objects.get()
-        self.c.get(reverse('social:cancel_fs_offer',
-                           kwargs={ 'offer_id': fso.id }))
-        self.acc2.reload()
-        self.assertEquals(self.acc2.fs_offers_inbox_count, 0)
-
-    def test_friendship_offer_declines_successfully(self):
-        self.c.login(username=self.acc2.username, password='123')
-        fso = FriendshipOffer.objects.get()
-        self.c.get(reverse('social:decline_fs_offer',
-                           kwargs={ 'offer_id': fso.id }))
-        self.assertEquals(FriendshipOffer.objects.count(), 0)
-
-    def test_friendship_offer_declining_decreases_fs_offers_inbox_count(self):
-        self.c.login(username=self.acc2.username, password='123')
-        fso = FriendshipOffer.objects.get()
-        self.c.get(reverse('social:decline_fs_offer',
-                           kwargs={ 'offer_id': fso.id }))
-        self.assertEquals(self.acc2.fs_offers_inbox_count, 0)
-
-
-
-class MassFriendshipTestCase(BasicTestCase):
-
-    def setUp(self):
-        FriendshipOffer.objects.delete()
-        super(MassFriendshipTestCase, self).setUp()
-
-    def tearDown(self):
-        FriendshipOffer.objects.delete()
-        super(MassFriendshipTestCase, self).tearDown()
-
-    def test_mass_friendship_limits(self):
-        FriendshipOffer.objects.delete()
-        c2 = Client()
-        print
-        for i in xrange(501):
-            if not i % 25:
-                print  '\rmass friending.. %002d%%' % (i*100/500),
-                sys.stdout.flush()
-            acc = User.create_user(username='masstest%s' % i,
-                                      password='123')
-            c2.login(username='masstest%s' % i, password='123')
-            acc.save()
-            self.c.get(reverse('social:friend', kwargs={ 'user_id': acc.id }))
-            c2.get(reverse('social:friend', kwargs={ 'user_id': self.acc1.id }))
-
-        # one last offer violates limits, so it's not accepted
-        self.assertEquals(FriendshipOffer.objects.count(), 1)
-
-        self.acc1.reload()
-
-        # no more than 500 mutual friends
-        self.assertEquals(len(self.acc1.mutual_friends), 500)
-'''
