ed3ad05d623c8155414f7bc3e8e7500a42c76e00
Ilya <elias@web-mark.ru>
2010-12-04 15:23:57 +0300

    Добавить singleton

---
+++ utils.py
@@ -0,0 +1,7 @@
+def singleton(fn):
+    def getinstance(self, *args, **kwarg):
+        cashed = '_%s' % fn.__name__
+        if not hasattr(self, cashed):
+            setattr(self, cashed, fn(self, *args, **kwarg))
+        return getattr(self, cashed)
+    return getinstance
