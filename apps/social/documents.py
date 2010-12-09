224b819442202a6c86019805694c648688f3dc61
Ilya <elias@web-mark.ru>
2010-12-09 11:33:37 +0300

    Изменить индекс username на email

--- apps/social/documents.py
+++ apps/social/documents.py
@@ -94,7 +94,7 @@ class User(Document):
         return filter(lambda x: x.is_online(),self.friends.list)
         
     meta = {
-        'indexes': ['username',]
+        'indexes': ['email',]
     }
 
 
