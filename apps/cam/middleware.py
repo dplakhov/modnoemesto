0ceb1c538734ff7910e3baeb3b01fb9d5671d6e7
dgk <dgk@web-mark.ru>
2010-12-10 18:50:31 +0300

    в правой колонке только включённые и публичные камеры

--- apps/cam/middleware.py
+++ apps/cam/middleware.py
@@ -3,5 +3,8 @@ from apps.cam.documents import Camera
 
 class PlaceBoxMiddleware:
     def process_request(self, request):
-        request.places = Camera.objects.order_by('-date_created')
+        request.places = Camera.objects(is_view_public=True,
+                                        is_view_enabled=True
+                                        ).order_by('-date_created')
         request.places_all_count = Camera.objects.count()
+        
