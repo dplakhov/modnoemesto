6bb157d6c2147d1a21ceae3bdf1390fde188892f
Ilya <elias@web-mark.ru>
2010-12-20 10:19:13 +0300

    Правка кол-ва камер в левой панели

--- apps/cam/middleware.py
+++ apps/cam/middleware.py
@@ -3,8 +3,7 @@ from apps.cam.documents import Camera
 
 class PlaceBoxMiddleware:
     def process_request(self, request):
-        request.places = Camera.objects(is_view_public=True,
-                                        is_view_enabled=True
-                                        ).order_by('-date_created')
+        request.places = Camera.objects
+        request.places = list(request.places)
         request.places_all_count = Camera.objects.count()
         
