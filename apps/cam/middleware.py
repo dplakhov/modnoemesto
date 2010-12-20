dae3d8c8ad0fa7d4caea753cd51ffc8076f5878b
Ilya <elias@web-mark.ru>
2010-12-20 12:31:58 +0300

    Поправить отображение камер в левой панели

--- apps/cam/middleware.py
+++ apps/cam/middleware.py
@@ -3,7 +3,9 @@ from apps.cam.documents import Camera
 
 class PlaceBoxMiddleware:
     def process_request(self, request):
-        request.places = Camera.objects
+        request.places = Camera.objects(is_view_public=True,
+                                        is_view_enabled=True
+                                        ).order_by('-date_created')[:7]
         request.places = list(request.places)
         request.places_all_count = Camera.objects.count()
         
