b36391e02a56c620667decb0d4f843cc4634226c
eugene <eugene@web-mark.ru>
2010-12-22 18:01:17 +0300

    added filters to logging view #60

--- apps/logging/views.py
+++ apps/logging/views.py
@@ -6,32 +6,55 @@ from django.conf import settings
 from models import LogEntry
 
 
+def get_loggers_names():
+    return LogEntry.objects.distinct('logger_name')
+
 @permission_required('logging')
 def log_list(request, page=1):
-    entries = LogEntry.objects.all()
+    entries = LogEntry.objects.order_by('-date')
+    
+    logger = request.GET.get('logger', None)
+    if logger:
+        entries = entries.filter(logger_name=logger)
+    
     try:
         paginator = Paginator(entries, settings.LOGS_PER_PAGE)
         page_entries = paginator.page(page)
     except (EmptyPage, InvalidPage):
         page_entries = paginator.page(paginator.num_pages)
 
+    loggers = get_loggers_names()
+
     return direct_to_template(request, 'logging/log_list.html',
-                              dict(page_entries=page_entries)
-                              )
+                              {
+                               'page_entries': page_entries,
+                               'loggers': loggers,
+                               'curren_logger': logger,
+                              })
 
 @permission_required('logging')
 def log_list_level(request, level=None, page=1):
     if level:
-        entries = LogEntry.objects.filter(levelname=level).all()
+        entries = LogEntry.objects.filter(levelname=level).order_by('-date')
     else:
         entries = LogEntry.objects.none()
+
+    logger = request.GET.get('logger', None)
+    if logger:
+        entries = entries.filter(logger_name=logger)
+
     try:
         paginator = Paginator(entries, settings.LOGS_PER_PAGE)
         page_entries = paginator.page(page)
     except (EmptyPage, InvalidPage):
         page_entries = paginator.page(paginator.num_pages)
     
+    loggers = get_loggers_names()
+
     return direct_to_template(request, 'logging/log_list.html',
-                              dict(page_entries=page_entries)
-                              )
+                              {
+                               'page_entries': page_entries,
+                               'loggers': loggers,
+                               'curren_logger': logger,                               
+                              })
     
