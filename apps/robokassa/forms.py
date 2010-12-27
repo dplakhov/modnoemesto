56ccedde794727a48f907e73f03583e2c4886e8c
Ilya <elias@web-mark.ru>
2010-12-27 17:04:06 +0300

    Поправить запрос в робокассе

--- apps/robokassa/forms.py
+++ apps/robokassa/forms.py
@@ -154,6 +154,6 @@ class SuccessRedirectForm(_RedirectPageForm):
         data = super(SuccessRedirectForm, self).clean()
         if STRICT_CHECK:
             user = UserId.get_user_by_id(data['InvId'])
-            if not user or not UserOrder.objects.filter(id=str(user.id)).count():
+            if not user or not UserOrder.objects.filter(user=ser).count():
                 raise forms.ValidationError(u'От ROBOKASSA не было предварительного уведомления')
         return data
