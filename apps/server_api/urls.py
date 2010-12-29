cd673f09e93f6f722ad177419eac85ebcf30bbe0
dgk <dgk@web-mark.ru>
2010-12-29 18:13:36 +0300

    API: списки друзей

--- apps/server_api/urls.py
+++ apps/server_api/urls.py
@@ -6,4 +6,8 @@ urlpatterns = patterns('apps.server_api.views',
     url(r'^notify/online/$', 'notify_online', name='notify_online'),
     url(r'^notify/offline/$', 'notify_offline', name='notify_offline'),
     url(r'^notify/reset/$', 'notify_reset', name='notify_reset'),
+
+    url(r'^user/(?P<id>[a-f0-9]{24})/friends/(?P<state>(online|all))\.(?P<format>(txt|xml))$',
+        'friend_list', name='friend_list'),
+
 )
