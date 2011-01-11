898a6a78ed8e4e24d348be200c15e775a96d94b6
Ilya <elias@web-mark.ru>
2011-01-11 16:10:12 +0300

    Применить общий пагинатор для списка заказов в админке

--- apps/billing/urls.py
+++ apps/billing/urls.py
@@ -11,7 +11,5 @@ urlpatterns = patterns('apps.billing.views',
     url(r'^tariffs/(?P<id>[a-f0-9]{24})/$', 'tariff_edit', name='tariff_edit'),
     url(r'^tariffs/(?P<id>[a-f0-9]{24})/delete/$', 'tariff_delete', name='tariff_delete'),
     url(r'^orders/$', 'order_list', name='order_list'),
-    url(r'^orders/(?P<page>\d+)/$', 'order_list', name='order_list_page'),
     url(r'^access_orders/$', 'access_order_list', name='access_order_list'),
-    url(r'^access_orders/(?P<page>\d+)/$', 'access_order_list', name='access_order_list_page'),
 )
