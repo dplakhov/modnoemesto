e7597bac1c64303afb96bcd0dc223b350fb580fd
Ilya <elias@web-mark.ru>
2011-01-11 15:49:55 +0300

    Применить общий пагинатор для списка юзеров в админке

--- apps/admin/urls.py
+++ apps/admin/urls.py
@@ -5,5 +5,4 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('apps.admin.views',
     url(r'^statistic/$', 'statistic', name='statistic'),
     url(r'^users/$', 'user_list', name='user_list'),
-    url(r'^users/(?P<page>\d+)/$', 'user_list', name='user_list_page'),
 )
