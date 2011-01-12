53e798e5781eaaffa5dc9b13815759dac208b477
eugene <eugene@web-mark.ru>
2011-01-12 19:26:00 +0300

    fix logging pagination

--- apps/logging/views.py
+++ apps/logging/views.py
@@ -10,39 +10,20 @@ def get_loggers_names():
     return LogEntry.objects.distinct('logger_name')
 
 @permission_required('logging')
-def log_list(request, page=1):
-    entries = LogEntry.objects.order_by('-date')
+def log_list(request, page=1, level=''):
+    filter_data = {}
 
     logger = request.GET.get('logger', None)
     if logger:
-        entries = entries.filter(logger_name=logger)
+        filter_data['logger_name'] = logger
 
-    try:
-        paginator = Paginator(entries, settings.LOGS_PER_PAGE)
-        page_entries = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        page_entries = paginator.page(paginator.num_pages)
-
-    loggers = get_loggers_names()
-
-    return direct_to_template(request, 'logging/log_list.html',
-                              {
-                               'page_entries': page_entries,
-                               'loggers': loggers,
-                               'current_logger': logger,
-                               'current_level': '',
-                              })
-
-@permission_required('logging')
-def log_list_level(request, level=None, page=1):
     if level:
-        entries = LogEntry.objects.filter(levelname=level).order_by('-date')
-    else:
-        entries = LogEntry.objects.none()
+        filter_data['levelname'] = level
 
-    logger = request.GET.get('logger', None)
-    if logger:
-        entries = entries.filter(logger_name=logger)
+    if filter_data:
+        entries = LogEntry.objects.filter(**filter_data).order_by('-date')
+    else:
+        entries = LogEntry.objects.order_by('-date')
 
     try:
         paginator = Paginator(entries, settings.LOGS_PER_PAGE)
