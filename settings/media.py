837d7863c6fbea7d086c5669737556d7fe43308d
dgk <dgk@web-mark.ru>
2011-01-13 13:26:11 +0300

    settings.media

---
+++ settings/media.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+AVATAR_SIZES = {
+    'avatar_midi.png': { 'width': 200, 'height': 200 },
+    'avatar_mini.png': { 'width': 60, 'height': 60 },
+    'avatar_micro.png': { 'width': 47, 'height': 47 },
+}
+
+SCREEN_SIZES = {
+    'camera_screen_full.png': { 'width': 515, 'height': 330 },
+    'camera_screen_normal.png': { 'width': 170, 'height': 109 },
+    'camera_screen_mini.png': { 'width': 80, 'height': 51 },
+}
+
+GROUP_PHOTO_SIZES = {
+    'group_photo_full.png': { 'width': 200, 'height': 200 },
+    'group_photo_normal.png': { 'width': 60, 'height': 60 },
+    'group_photo_mini.png': { 'width': 47, 'height': 47 },
+}
