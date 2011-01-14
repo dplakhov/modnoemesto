1991bf336e4e223a1e0a2a5692c89e34dc10f250
dgk <dgk@web-mark.ru>
2011-01-14 17:37:02 +0300

    возможность загружать список контактов друзей на странице рассылки приглашений - google csv #154

--- apps/invite/views.py
+++ apps/invite/views.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import csv
+import codecs
+from apps.social.documents import User
+
+import vobject
+import chardet
+
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.views.generic.simple import direct_to_template
@@ -32,23 +39,30 @@ def send(request):
         contacts = []
 
         if import_form.cleaned_data['type'] == 'csv':
-            import csv
-            reader = csv.reader(buffer)
-            for i, row in enumerate(reader):
-                print i, row
-                if not i:
-                    pass
-                else:
-                    pass
+            encoding = chardet.detect(buffer.read())['encoding']
+            buffer.reset()
+            reader = codecs.getreader(encoding)(buffer)
+            buffer = StringIO()
+            buffer.write('\n'.join(reader.readlines()).encode('utf-8'))
+            buffer.reset()
+
+            reader = csv.DictReader(buffer)
+            for row in reader:
+                email = row['E-mail Address']
+                first_name = row['First Name']
+                last_name = row['Last Name']
+                contacts.append((email, ('%s %s' %
+                                        (first_name, last_name)).decode('utf-8')))
+
         elif import_form.cleaned_data['type'] == 'vcard':
-            import vobject
             vobjects = vobject.readComponents(buffer)
 
             while 1:
                 try:
                     vcf = vcfs.next()
                     if vcf.contents.has_key('email'):
-                        contacts.append((vcf.email.value,vcf.fn.value))
+                        contacts.append((vcf.email.value,
+                                         vcf.fn.value))
 
                 except:
                     break
@@ -56,24 +70,34 @@ def send(request):
         else:
             raise NotImplementedError
 
+        sent_count = 0
         for (email, name) in contacts:
-            _send_invite(request.user, email, name)
+            if _send_invite(request.user, email, name):
+                sent_count += 1
 
         messages.add_message(request, messages.SUCCESS,
-                                 _('Invites sent'))
+                                 _('%(sent_count)d invites sent') %
+                                 dict(sent_count=sent_count))
 
-    return direct_to_template(request, 'social/invite_send.html',
+    return direct_to_template(request, "invite/send.html",
                               dict(send_form=send_form,
                                    import_form=import_form
                                    ))
 
 
 def _send_invite(sender, email, name):
+    if User.objects(email=email).count():
+        return False
+
     invite = Invite(sender=sender,
                 recipient_email=email,
                 recipient_name=name)
+
     invite.save()
     invite.send()
+
+    return True
+
 
 
 def invite(request, invite_id):
