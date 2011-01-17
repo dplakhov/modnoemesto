381c4a6b7469945c9bd54c4920035c2db33ba4f3
Ilya <elias@web-mark.ru>
2011-01-17 18:07:51 +0300

    Добавить ссылку на заметки пользователя #159

--- apps/notes/urls.py
+++ apps/notes/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('apps.notes.views',
     url(r'^$', 'note_list', name='note_list'),
+    url(r'^(?P<id>[a-f0-9]{24})/$', 'note_list', name='note_list'),
     url(r'^add/$', 'note_edit', name='note_add'),
 
     url(r'^(?P<note_id>[a-f0-9]{24})/$', 'note_view', name='note_view'),
