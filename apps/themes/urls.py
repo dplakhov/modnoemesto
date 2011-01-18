b8f77180d54456764098909fb0726d85baafc144
dgk <dgk@web-mark.ru>
2011-01-18 12:20:08 +0300

    возможность менять скин домашней страницы #153 - views

--- apps/themes/urls.py
+++ apps/themes/urls.py
@@ -4,4 +4,8 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('apps.themes.views',
    url(r'^(?P<theme_id>[a-f0-9]{24})/(?P<file_name>[\w\.]+)$',
        'file_view', name='file_view'),
+   url(r'^all/$', 'list', name='list'),
+   url(r'^add/$', 'add', name='add'),
+   url(r'^(?P<theme_id>[a-f0-9]{24})/delete/$', 'delete', name='delete'),
+
 )
--- apps/themes/views.py
+++ apps/themes/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse, Http404
+from django.views.generic.simple import direct_to_template
 
 from mongoengine.django.shortcuts import get_document_or_404
 
 from .documents import Theme
+from .forms import ThemeAddForm
 
 def file_view(request, theme_id, file_name):
     theme = get_document_or_404(Theme, id=theme_id)
@@ -17,3 +19,12 @@ def file_view(request, theme_id, file_name):
 
     response['Last-Modified'] = file.file.upload_date
     return response
+
+def add(request):
+    form = ThemeAddForm(request.POST or None, request.FILES)
+    return direct_to_template(request,
+                              'themes/list.html',
+                              dict(form=form)
+                              )
+def list(request):
+    pass
