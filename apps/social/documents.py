ff403882cb9ebeba736a2057a1436756e9d3743f
dgk <dgk@web-mark.ru>
2011-01-26 16:05:19 +0300

    Profile.for_html() += mobile_code, mobile_number

--- apps/social/documents.py
+++ apps/social/documents.py
@@ -76,6 +76,8 @@ class Profile(Document):
         self.sex = dict(ChangeProfileForm.SEX_CHOICES).get(self.sex, ChangeProfileForm.SEX_CHOICES[0][1])
         if self.mobile:
             self.mobile = "+7 (%s) %s %s %s" % (self.mobile[:3], self.mobile[3:6], self.mobile[6:8], self.mobile[8:])
+            self.mobile_code = self.mobile[:3]
+            self.mobile_number = self.mobile[3:]
         return self
 
 
