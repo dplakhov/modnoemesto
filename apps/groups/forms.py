787a311fadda3a87a29a2065bdf42b132322649b
dgk <dgk@web-mark.ru>
2011-01-31 19:25:41 +0300

    Group.timestamp

--- apps/groups/forms.py
+++ apps/groups/forms.py
@@ -17,7 +17,7 @@ class GroupCreationForm(forms.Form):
     public = forms.BooleanField(label=_('Access'), required=False)
     has_video_conference = forms.BooleanField(label=_('Has video conference'),
                                               required=False)
-    timestamp = forms.DateField(label=_('Timestamp'), required=False)
+    timestamp = forms.DateTimeField(label=_('Timestamp'), required=False)
 
     def __init__(self, *args, **kwarg):
         initial = kwarg.get('initial')
