f3e18ef81bc4923fd819e2bef9e350f19085325c
dgk <dgk@web-mark.ru>
2011-02-02 17:24:56 +0300

    datetime picker

--- settings/dist.py
+++ settings/dist.py
@@ -70,7 +70,7 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/admin/'
+ADMIN_MEDIA_PREFIX = '/admin-media/'
 
 LOGIN_URL = '/login/'
 
